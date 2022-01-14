9883a060874e361ea154154c47abb3a22843ea1c
Vitaly Zakatov <answer-0885@mail.ru>
2022-01-14 22:41:03 +0700

    Модальное окно

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,6 +5,7 @@ const modal = () => {
    const modalClose = document.querySelector('.header-modal__close');
    const overlay = document.querySelector('.overlay');
 
+
    modalBtn.addEventListener('click', () => {
       modalWindow.style.display = 'flex';
       overlay.style.display = 'flex';
@@ -13,6 +14,13 @@ const modal = () => {
    modalClose.addEventListener('click', () => {
       modalWindow.style.display = 'none';
       overlay.style.display = 'none';
+   });
+
+   overlay.addEventListener('click', (e) => {
+      if (!e.target.closest('.header-modal') || e.target.classList.contains('.header-modal__close')) {
+         modalWindow.style.display = 'none';
+         overlay.style.display = 'none';
+      }
    });
 
 
